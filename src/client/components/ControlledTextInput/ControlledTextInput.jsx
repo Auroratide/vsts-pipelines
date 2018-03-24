d918709ce3f1dc91e579ea435f9319f457b1f78a
auroratide <tf.auroratide@gmail.com>
2018-03-24 17:09:08 -0500

    [#1] Styling controlled text input

--- src/client/components/ControlledTextInput/ControlledTextInput.jsx
+++ src/client/components/ControlledTextInput/ControlledTextInput.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import c from 'classnames';
 
+import styles from './ControlledTextInput.style.css';
+
 const ControlledTextInput = ( {
   value,
   onChange,
   label,
   className
-} ) => <div className={c(className)}>
+} ) => <div className={c(styles['controlled-text-input'], className)}>
   {label && <label>{label}</label>}
   <input type='text' value={value} onChange={onChange} />
 </div>;
